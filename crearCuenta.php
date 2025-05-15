a6a754759084b378d008dd1a4b14c39d94323c0a
Mar <mariana.montejo04@gmail.com>
2025-05-15 14:15:54 -0600

    Vista "Registrarse" terminada

--- crearCuenta.php
+++ crearCuenta.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
     <link href="./estilos/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link href="./estilos/style.css" rel="stylesheet">
 </head>
 
@@ -16,19 +17,20 @@
             <div class="d-flex align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li>
-                        <a class="nav-link px-2 text-white" href="./index.php">Cartelera</a>
+                        <a class="nav-link px-2 text-white fs-5" href="./index.php">Cartelera</a>
                     </li>
                     <li>
-                        <a class="nav-link px-2 text-white" href="">|</a>
+                        <a class="nav-link px-2 text-white fs-5" href="">|</a>
                     </li>
                     <li>
-                        <a class="nav-link px-2 text-white" href="">Promociones</a>
+                        <a class="nav-link px-2 text-white fs-5" href="">Promociones</a>
                     </li>
                 </ul>
-                <a class="navbar-brand px-2 text-white" href="#">
-                    VERTIGO FILMS
-                    <img src="./media/LOGOVERTIGO.PNG" width="70" height="70">
+                <a class="navbar-brand px-2 text-center text-white d-block " href="#">
+                    <span class="d-block h4 mb-0 fw-bold">VÉRTIGO</span>
+                    <span class="d-block h5">FILMS</span>
                 </a>
+                <img src="./media/LOGOVERTIGO.PNG" width="70" height="70" class="ms-3">
             </div>
         </div>
     </header>
@@ -36,7 +38,7 @@
     <div class="row justify-content-md-center">
         <div class="col-md-5 col-lg-4">
             <div>
-                <h1 class="h1 text-center mb-3 mt-3">Registrarse</h1>
+                <h1 class="h1 text-center mb-3 mt-3 bold">Registrarse</h1>
             </div>
             <form method="POST" action="./logica/crearCuenta.php">
                 <div class="row g-3">
@@ -79,9 +81,23 @@
     </div>
 
 
-    <footer class="bd-footer py-4 py-md-5 mt-5 gradient-custom">
-        <div class="container py-4 py-md px-4 px-md-3 text-white">
+    <footer class="bd-footer py-4 mt-5 gradient-custom">
+        <div class="container text-white">
+            <div class="row align-items-center">
+                <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                    <img src="./media/LOGOVERTIGO.PNG" alt="Logo Vértigo" width="50" height="50">
+                    <span class="mb-3 mb-md-0">© 2025 Vértigo Films, Inc</span>
+                </div>
 
+                <div class="col-md-6 text-center text-md-end">
+                    <a href="https://www.instagram.com" target="_blank" class="text-white me-3">
+                        <i class="bi bi-instagram" style="font-size: 1.5rem;"></i>
+                    </a>
+                    <a href="https://www.facebook.com" target="_blank" class="text-white">
+                        <i class="bi bi-facebook" style="font-size: 1.5rem;"></i>
+                    </a>
+                </div>
+            </div>
         </div>
     </footer>
 
