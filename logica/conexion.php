50110c8beca1fc2ea84e28d8f3366db1a0265d3d
AlnOtzar”git config --global user.email [200~aotzar2003@gmail.com” <aotzar2003@gmail.com>
2025-05-15 23:11:41 -0600

    cartelera revision v1.1

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,18 +1,11 @@
 <?php
 
 //local Alan
-<<<<<<< HEAD
 $host_db = "localhost:3306";
 $user_db = "root";
 $pass_db = "ImpliedFiber356@";
 $db_name = "cine_VertigoDB";
 
-=======
-//$host_db = "localhost:3306";
-//$user_db = "root";
-//$pass_db = "ImpliedFiber356@";
-//$db_name = "cine_VertigoDB";
->>>>>>> 44b259274a2d349c40896be1d232d061e58825a1
 //local AlN
 //$host_db = "localhost:3306";
 //$user_db = "root";
@@ -20,30 +13,21 @@ $db_name = "cine_VertigoDB";
 //$db_name = "cine_VertigoDB";
 
 //Hosting
-$host_db = "localhost";
-$user_db = "root";
-$pass_db = "halo";
-$db_name = "cine_VertigoDB";
+// $host_db = "localhost";
+// $user_db = "root";
+// $pass_db = "halo";
+// $db_name = "cine_VertigoDB";
 
 //Servidor
 //$host_db = "localhost";
 //$user_db = "root";
 //$pass_db = "123456789";
 //$db_name = "carrito";
-<<<<<<< HEAD
 
 // $host_db = "localhost";
 // $user_db = "root";
 // $pass_db = "halo";
 // $db_name = "pagina_cine";
-=======
-/*
-$host_db = "localhost";
-$user_db = "root";
-$pass_db = "halo";
-$db_name = "pagina_cine";
-*/
->>>>>>> 44b259274a2d349c40896be1d232d061e58825a1
 
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
