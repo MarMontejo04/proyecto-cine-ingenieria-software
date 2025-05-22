07426b75e5f4e956a3cc18ca7f08ef01f91ba076
AleS117 <haloprom091@gmail.com>
2025-05-22 10:22:46 -0600

    Correxcion de Asentos

--- logica/conexion.php
+++ logica/conexion.php
@@ -15,17 +15,17 @@
 
 
 // Local Alan
-$host_db = "localhost:3306";
-$user_db = "root";
-$pass_db = "ImpliedFiber356@";
-$db_name = "cine_VertigoDB";
+//$host_db = "localhost:3306";
+//$user_db = "root";
+//$pass_db = "ImpliedFiber356@";
+//$db_name = "cine_VertigoDB";
 
 
 //Hosting
- //$host_db = "localhost";
-// $user_db = "root";
-// $pass_db = "halo";
- //$db_name = "cine_VertigoDB";
+$host_db = "localhost";
+$user_db = "root";
+$pass_db = "halo";
+$db_name = "cine_VertigoDB";
 
 //Servidor
 //$host_db = "localhost";
@@ -46,6 +46,8 @@ $db_name = "cine_VertigoDB";
 
 
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
+
+$conexion->set_charset("utf8mb4");
 
 if($conexion->connect_error){
     echo"<h1>MySQL no le  esta dando permisos para ejecutar consultas verificar error</h1>";
